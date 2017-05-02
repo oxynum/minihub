debcedc324b1814a1ec53a057b66e2c18ff2db88
Simbadev <maxime.gasnier.1@gmail.com>
2017-05-02 14:59:25 +0200

    Add stable tickets with printer -> passed test

--- minihub.js
+++ minihub.js
@@ -1,12 +1,8 @@
-const MiniPrinterUsb     = require('./mini-printer/miniPrinterUsb').MiniPrinterUsb,
-      MiniPrinterNetwork = require('./mini-printer/miniPrinterNetwork').MiniPrinterNetwork,
-      MiniPrinterSerial  = require('./mini-printer/miniPrinterSerial').MiniPrinterSerial,
-      express            = require('express'),
+const express            = require('express'),
       bodyParser         = require('body-parser');
 
-let app        = express(),
-    testRoutes = require('./routes/testTicketsRoutes'),
-    api        = require('./routes/ticketsRoutes');
+let app           = express(),
+    miniPrinter = require('./routes/mini-printer/routes-mini-printer');
 
 // [CONFIG]
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
@@ -18,10 +14,9 @@ app.use((req, res, next) => {
 }); // Allow Cross Origin access
 
 // [USED ROUTES]
-app.use('/mm/tests', testRoutes);
-app.use('/printer/mm/api', api);
+app.use('/mm/plugins', miniPrinter);
 
 // [LISTENING PORT]
-app.listen(7001, () => {
+app.listen(3001, () => {
   console.log('Printer System is connected on port: ' + 3001);
 });
