f29ffdc441c07b183ac850fdd53c16dea5c85fa2
Simbadev <maxime.gasnier.1@gmail.com>
2017-06-21 15:04:22 +0200

    add js to html module and add it to ticket template

--- plugins/mini-mail/templates/ticketMailTemplate.js
+++ plugins/mini-mail/templates/ticketMailTemplate.js
@@ -1,10 +1,22 @@
+const HTML = require('js-to-html').html;
 
+/**
+ * Class representing a TicketMailTemplate.
+ * Will allow us to create as many Tickets as we want according to templates in html.
+ * This class is using js-to-html to produce the work.
+ */
 class TicketMailTemplate {
-  
+  /**
+   * Create a Ticket Mail template.
+   */
   constructor() {
 
   }
 
+  /**
+   * Will generate a new HTML ticket to send via mail. 
+   * @param {Object} ticket - that will be analyzed to create html content.
+   */
   generate(ticket) {
     return "<b> " + ticket + " </b>";
   }
